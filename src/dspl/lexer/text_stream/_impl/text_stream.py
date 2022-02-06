14ffa37d5b09146d6b883c789174d7504b943a44
David Strootman <david.strootman@gmail.com>
2022-02-06 20:25:10 +0100

    Add docstrings to text_stream

--- src/dspl/lexer/text_stream/_impl/text_stream.py
+++ src/dspl/lexer/text_stream/_impl/text_stream.py
@@ -9,28 +9,41 @@ from dspl.helper import flatten_right, mapx, rec_seq, split_last
 
 class TextStream:
     """
-    TextStream is a static wrapper around the string, "guaranteeing" no side-effects.
+    A textstream wraps a string, and provides methods which have no side effects.
     """
 
-    def __init__(self, text: str):
+    def __init__(self, text: str) -> "TextStream":
         self._text: str = text
 
     def grab(self, count: int = 1) -> tuple[str, "TextStream"]:
         """
-        Grabs the first count values, calling __next__ on self count times.
+        Splits the textstream into a string of count length, and the new modified TextStream.
+
+        :param count: The amount of characters to split from the start.
+        :return: count characters and a new modified TextStream.
         """
         return self._text[0:count], TextStream(self._text[count:])
 
     def peek(self, count=1) -> str:
+        """
+        Peek this TextStream without modifying it.
+
+        :param count: The amount of characters to peek from the start.
+        :return: The peeked characters.
+        """
         if not self._text:
             return ""
 
         return self._text[0:count]
 
 
-def grab_until(pred: Callable[(str), bool], stream: TextStream) -> rec_seq:
+def grab_until(pred: Callable[(str), bool], stream: TextStream) -> Tuple[str, TextStream]:
     """
-    Grab chars from the text string until the delim is reached (exclusive).
+    Grab chars from the beginning of the TextStream until the predicate is true for the next character (exclusive).
+
+    :param pred: The predicate which breaks the loop once it is True for the next character.
+    :param stream: The TextStream to grab from.
+    :return: The grabbed string and a new modified TextStream
     """
 
     def _grab_until_internal(pred: Callable[(str), bool], stream: "TextStream"):
