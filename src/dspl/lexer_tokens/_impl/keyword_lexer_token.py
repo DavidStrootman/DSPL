1261c8107f13317965840cc1194190b8d8cf8d0d
David Strootman <david.strootman@gmail.com>
2022-02-06 20:47:18 +0100

    Add docstrings for keyword_lexer_token

--- src/dspl/lexer_tokens/_impl/keyword_lexer_token.py
+++ src/dspl/lexer_tokens/_impl/keyword_lexer_token.py
@@ -17,13 +17,19 @@ class KeywordLexerTokenKind(ValuableEnum):
 
 class KeywordLexerToken(LexerToken):
     """
-    Keyword lexer tokens are created from RawIdentLexerTokens
+    Keyword lexer tokens describe protected keyword identifiers, such as fn which denotes a function.
     """
     def __init__(self, kind: KeywordLexerTokenKind):
         super().__init__(kind, kind.value)
 
     @staticmethod
     def from_raw_ident(ident: "RawIdentLexerToken") -> Union["KeywordLexerToken", "RawIdentLexerToken"]:
+        """
+        Create a keywords from a RawIdentToken. This happens as a second step after lexing, during token expansion.
+
+        :param ident: The identifier to try cast into a keyword.
+        :return: Either a keyword token if succeeds, otherwise the original identifier token.
+        """
         if ident.value in KeywordLexerTokenKind.values():
             return KeywordLexerToken(KeywordLexerTokenKind(ident.value))
         return ident
